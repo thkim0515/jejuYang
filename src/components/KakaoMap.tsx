f19ad29ca79dd58bc71bc75a5afd10d8d122e8c5
thkim0515 <thkim0515@naver.com>
2025-06-19 23:50:30 +0900

    1

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -26,7 +26,13 @@ const KakaoMap = () => {
       document.head.appendChild(script);
     });
   };
+  
+  useEffect(() => {
+    console.log("✅ KAKAO JS KEY:", process.env.REACT_APP_KAKAO_JS_KEY);
+    console.log("✅ KAKAO REST KEY:", process.env.REACT_APP_KAKAO_REST_KEY);
+  }, []);
 
+  
   useEffect(() => {
     loadKakaoMapScript().then(() => {
       window.kakao.maps.load(() => {
